946bb02e40bd4776a7339690f4acc20ee4864f8f
AzizaMosbah <121355473+AzizaMosbah@users.noreply.github.com>
2024-05-13 18:00:07 +0300

    Add files via upload

--- js/denta.js
+++ js/denta.js
@@ -145,3 +145,91 @@ signBtns.forEach((btn, index) => {
 
 
 
+const dropArea = document.querySelector(".drag-area");
+    const dragText = document.getElementById("header");
+
+    let button = dropArea.querySelector(".button");
+    let input = dropArea.querySelector("input");
+    let file;
+
+    button.onclick = () => {
+      input.click();
+    };
+    // when browse
+    input.addEventListener("change", function () {
+      file = this.files[0];
+      dropArea.classList.add("active");
+      displayFile();
+    });
+    // when file is inside drag area
+    dropArea.addEventListener("dragover", (event) => {
+      event.preventDefault();
+      dropArea.classList.add("active");
+      dragText.textContent = "Release to Upload";
+      // console.log('File is inside the drag area');
+    });
+    // when file leave the drag area
+    dropArea.addEventListener("dragleave", () => {
+      dropArea.classList.remove("active");
+      // console.log('File left the drag area');
+      dragText.textContent = "Drag & Drop";
+    });
+    // when file is dropped
+    dropArea.addEventListener("drop", (event) => {
+      event.preventDefault();
+      // console.log('File is dropped in drag area');  file = event.dataTransfer.files[0]; // grab single file even of user selects multiple files
+      // console.log(file);
+      displayFile();
+    });
+
+    function displayFile() {
+    let fileType = file.type;
+    // console.log(fileType);  
+    let validExtensions = ["image/jpeg", "image/jpg", "image/png"];  
+    if (validExtensions.includes(fileType)) {
+        // console.log('This is an image file');
+        let fileReader = new FileReader();    
+        fileReader.onload = () => {
+            let fileURL = fileReader.result;
+            // console.log(fileURL);
+            let imgTag = `<img src="${fileURL}" alt="">`;
+            dropArea.innerHTML = imgTag;
+        };
+        fileReader.readAsDataURL(file);
+    } else {
+        alert("This is not an Image File");
+        dropArea.classList.remove("active");
+    }
+}
+
+
+
+
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  var icons = document.querySelectorAll('.icon');
+  icons.forEach(function(icon) {
+    icon.addEventListener('click', function() {
+      var target = this.getAttribute('data-target');
+      var contents = document.querySelectorAll('.content');
+      
+      contents.forEach(function(content) {
+        if (content.classList.contains(target)) {
+          content.classList.add('active');
+          content.classList.remove('inactive');
+        } else {
+          content.classList.remove('active');
+          content.classList.add('inactive');
+        }
+      });
+
+      icons.forEach(function(icon) {
+        icon.classList.remove('active');
+      });
+
+      this.classList.add('active');
+    });
+  });
+});
